9ef57d1faadbb2e007c505ba408b39e067e4e4e6
tom <tomallpress@hotmail.com>
2018-01-30 12:04:24 +0000

    replaced "magic numbers " with constants

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,44 +1,48 @@
-require File.join(File.dirname(__FILE__), 'item') 
+require File.join(File.dirname(__FILE__), 'item')
 
 class GildedRose
+
+  NORMAL_ITEM_MAX_QUALITY = 50
+  LEGENDARY_ITEM_MAX_QUALIRY = 80
+  ITEM_MIN_QUALITY = 0
 
   def initialize(items)
     @items = items
   end
 
-  def update_quality()
+  def update_quality
+
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+
+      if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert')
         if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
+          if item.name != 'Sulfuras, Hand of Ragnaros'
             item.quality = item.quality - 1
           end
         end
       else
-        if item.quality < 50
+        if item.quality < NORMAL_ITEM_MAX_QUALITY
           item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
+          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
             if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+              item.quality = item.quality + 1 if item.quality < NORMAL_ITEM_MAX_QUALITY
             end
             if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+              item.quality = item.quality + 1 if item.quality < NORMAL_ITEM_MAX_QUALITY
             end
           end
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
+
+      if item.name != 'Sulfuras, Hand of Ragnaros'
         item.sell_in = item.sell_in - 1
       end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
+
+      if item.sell_in < ITEM_MIN_QUALITY
+        if item.name != 'Aged Brie'
+          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
+            if item.quality > ITEM_MIN_QUALITY
+              if item.name != 'Sulfuras, Hand of Ragnaros'
                 item.quality = item.quality - 1
               end
             end
@@ -46,11 +50,10 @@ class GildedRose
             item.quality = item.quality - item.quality
           end
         else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+          item.quality = item.quality + 1 if item.quality < NORMAL_ITEM_MAX_QUALITY
         end
       end
+
     end
   end
 end
--- texttest_fixture.rb
+++ texttest_fixture.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby -w
 
-require File.join(File.dirname(__FILE__), '../lib/gilded_rose')
-require File.join(File.dirname(__FILE__), '../lib/item')
+require File.join(File.dirname(__FILE__), './lib/gilded_rose')
+require File.join(File.dirname(__FILE__), './lib/item')
 
 
 puts "OMGHAI!"
