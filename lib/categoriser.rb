4cea19070109cac7ae7d26e32d8009e583e67278
tom <tomallpress@hotmail.com>
2018-01-31 10:54:14 +0000

    extracted catergorise methods in gilded rose inot a module called categoriser

---
+++ lib/categoriser.rb
@@ -0,0 +1,51 @@
+require File.join(File.dirname(__FILE__), 'item')
+require File.join(File.dirname(__FILE__), 'aged_brie')
+require File.join(File.dirname(__FILE__), 'backstage_pass')
+require File.join(File.dirname(__FILE__), 'conjured_item')
+require File.join(File.dirname(__FILE__), 'normal_item')
+require File.join(File.dirname(__FILE__), 'sulfuras_item')
+
+module Categoriser
+
+    def categorise_items
+      @items.each do |item|
+        if isAgedBrie?(item)
+          to_add = AgedBrie.new(item)
+        elsif isSulfuras?(item)
+          to_add = SulfurasItem.new(item)
+        elsif isBackstagePass?(item)
+          to_add = BackstagePass.new(item)
+        elsif isConjured?(item)
+          to_add = ConjuredItem.new(item)
+        elsif isNormalItem?(item)
+          to_add = NormalItem.new(item)
+        end
+        @categorised_items.push(to_add)
+      end
+    end
+
+    def isAgedBrie?(item)
+      item.name == 'Aged Brie' ? true : false
+    end
+
+    def isSulfuras?(item)
+      item.name.include?("Sulfuras") ? true : false
+    end
+
+    def isBackstagePass?(item)
+      item.name.include?("Backstage pass") ? true : false
+    end
+
+    def isConjured?(item)
+      item.name.include?("Conjured") ? true : false
+    end
+
+    def isNormalItem?(item)
+      if (isAgedBrie?(item) || (isSulfuras?(item) || (isBackstagePass?(item) || isConjured?(item))))
+        return false
+      else
+        return true
+      end
+    end
+
+end
--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,11 +1,10 @@
-require File.join(File.dirname(__FILE__), 'item')
-require File.join(File.dirname(__FILE__), 'aged_brie')
-require File.join(File.dirname(__FILE__), 'backstage_pass')
-require File.join(File.dirname(__FILE__), 'conjured_item')
-require File.join(File.dirname(__FILE__), 'normal_item')
-require File.join(File.dirname(__FILE__), 'sulfuras_item')
+require File.join(File.dirname(__FILE__), 'categoriser')
 
 class GildedRose
+
+  attr_reader :categorised_items
+
+  include Categoriser
 
   NORMAL_ITEM_MAX_QUALITY = 50
   LEGENDARY_ITEM_MAX_QUALIRY = 80
@@ -20,49 +19,8 @@ class GildedRose
 
   def update_quality
     categorise_items()
-    @categorised_items.each do |item|
+    categorised_items.each do |item|
       item.alter()
-    end
-  end
-
-  def categorise_items
-    @items.each do |item|
-      if isAgedBrie?(item)
-        to_add = AgedBrie.new(item)
-      elsif isSulfuras?(item)
-        to_add = SulfurasItem.new(item)
-      elsif isBackstagePass?(item)
-        to_add = BackstagePass.new(item)
-      elsif isConjured?(item)
-        to_add = ConjuredItem.new(item)
-      elsif isNormalItem?(item)
-        to_add = NormalItem.new(item)
-      end
-      @categorised_items.push(to_add)
-    end
-  end
-
-  def isAgedBrie?(item)
-    item.name == 'Aged Brie' ? true : false
-  end
-
-  def isSulfuras?(item)
-    item.name.include?("Sulfuras") ? true : false
-  end
-
-  def isBackstagePass?(item)
-    item.name.include?("Backstage pass") ? true : false
-  end
-
-  def isConjured?(item)
-    item.name.include?("Conjured") ? true : false
-  end
-
-  def isNormalItem?(item)
-    if (isAgedBrie?(item) || (isSulfuras?(item) || (isBackstagePass?(item) || isConjured?(item))))
-      return false
-    else
-      return true
     end
   end
 
